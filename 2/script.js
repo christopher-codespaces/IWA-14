ec3e286a63a1da80f2a567d686c5ac5c1fc361f5
christopher-codespaces <125880140+christopher-codespaces@users.noreply.github.com>
2023-04-19 14:37:31 +0000

    I removed the second argument b from the multiply function because it only needs to multiply one value, not two. Additionally, since we are not actually multiplying c by any other value in the internal function, we can simply return the input value c directly.

--- 2/script.js
+++ 2/script.js
@@ -1,42 +1,35 @@
-function add(a, b) {
-    return a + b;
-  }
-  
-  function multiply(c) {
-    return c;
-  }
-  
-  function internal() {
-    const added = this.add(this.internal.a, this.internal.b);
-    const multiplied = this.multiply(this.internal.c);
-    const value = multiplied * added
-    return console.log(value);
-  }
-  
-  // Not allowed to change below this
-  
-  const example1 = {
-    internal: {
-      a: 2,
-      b: 4,
-      c: 8,
-    },
-    add,
-    multiply,
-    calculate: internal,
-  };
-  
-  const example2 = {
-    internal: {
-      a: 2,
-      b: 2,
-      c: 3,
-    },
-    add,
-    multiply,
-    calculate: internal,
-  };
-  
- example1.calculate();
- example2.calculate();
-  
+const add = (a, b) => { return a + b }
+const multiply = (c) => { return c }
+
+function internal() {
+  const added = this.add(this.internal.a, this.internal.b)
+  const multiplied = this.multiply(this.internal.c)
+  return added * multiplied
+}
+
+// Not allowed to change below this
+
+const example1 = {
+  internal: {
+    a: 2,
+    b: 4,
+    c: 8,
+  },
+  add,
+  multiply,
+  calculate: internal
+}
+
+const example2 = {
+  internal: {
+    a: 2,
+    b: 2,
+    c: 3,
+  },
+  add,
+  multiply,
+  calculate: internal
+}
+
+console.log(example1.calculate()) // 48
+console.log(example2.calculate()) // 12
